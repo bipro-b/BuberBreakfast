09df917797fca941ae3460598069db5c82d14502
bipro <biprocuet@gmail.com>
2023-12-08 17:10:32 +0600

    define routes

--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83ca393bd402ef1878e8fd94f3b87ff3a331946d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9dab40a214b663f019a7322457eca922deabda")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
